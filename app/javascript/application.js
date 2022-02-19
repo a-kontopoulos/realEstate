6590c245b9ee44fd1a599b3cab73bba1e3c60741
a-kontopoulos <a.kontopoulos@gmail.com>
2022-02-19 06:29:19 -0600

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+// Include bootstrap in the app
+import "popper"
+import "bootstrap"
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'static_pages#index'
+  get 'static_pages/services'
+  get 'static_pages/contact'
+end
