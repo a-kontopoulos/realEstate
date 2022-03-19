7792133f8d06bed046ef6e49b9e30993f8f04c22
a-kontopoulos <a.kontopoulos@gmail.com>
2022-03-19 13:27:03 -0500

    work on login

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
   get 'static_pages/services'
   get 'static_pages/contact'
 
+  get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
-  #route to signup new user
+  # route to signup new user
   get 'signup', to: 'users#new'
 
 end
